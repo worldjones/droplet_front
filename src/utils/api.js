57d3dfae10dcc4b3a9c3f11c50de73a1bacda822
worldjones <worldjones@gmail.com>
2021-06-15 02:26:35 +0200

    changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_MAIN = axios.create({
-	baseURL: "https://3sem.dyrhoi.com/startcode-v2/api/",
+	baseURL: "http://worldjones.dk/startcode/api/",
 	headers: {
 		"Content-type": "application/json",
 		Accept: "application/json",
